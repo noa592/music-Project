c426c3e630649dc886125c62eef4be17349af29b
noa592 <noabenyakov123@gmail.com>
2025-01-12 10:59:25 +0200

    hiiiccbgvf

--- app/src/main/java/com/example/noa_project/BlankFragment2.java
+++ app/src/main/java/com/example/noa_project/BlankFragment2.java
@@ -16,5 +16,6 @@ public class BlankFragment2 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blank2, container, false);
+        /////
     }
 }
